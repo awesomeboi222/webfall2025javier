1b01cd1dca4952cff883175c30d3d9006b5b8a68
awesomeboi222 <153134162+awesomeboi222@users.noreply.github.com>
2025-11-06 15:04:08 -0800

    Add files via upload

---
+++ jss_demo/js/script.js
@@ -0,0 +1,8 @@
+function askQuestion() {
+    var p = prompt('how much wood would a wood chuck chuck if a wood chuck could chuck wood?');
+    if (p !=null) {
+        document.getElementById
+        ('question').innerHTML =
+        'damn twin ' + p + ' woods' + ' is a lotta wood';
+    }
+}
